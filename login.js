80c69cb327cd719db6c868833ce0098f8696bede
AstaCarl <cph-ac300@cphbusiness.dk>
2024-10-02 21:10:40 +0200

    Refactor login.js: Remove unnecessary code for form input conditions

--- login.js
+++
@@ -1,6 +0,0 @@
-  
-  // conditions for the forminputs
-  const emailCondition =
-    email.length < 16 || !email.includes("@");
-  const passwordCondition = newPassword.length < 8;
-  const usernameCondition = newUser.length < 4 || newUser.length > 30;
